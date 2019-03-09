b6938175566d742cb4efcaae63ea3b1b8fc4954a
Vitalie Spinu <spinuvit@gmail.com>
2019-03-09 20:40:43 +0100

    Use \\' instead of $ in auto-mode-alist regexp

--- poly-R.el
+++ poly-R.el
@@ -706,13 +706,13 @@ The Rmd yaml preamble must contain runtime: shiny declaration."
 
 ;;; ASSOCIATIONS
 
-(add-to-list 'auto-mode-alist '("\\.Snw$" . poly-noweb+r-mode))
-(add-to-list 'auto-mode-alist '("\\.[rR]nw$" . poly-noweb+r-mode))
-(add-to-list 'auto-mode-alist '("\\.[rR]md$" . poly-markdown+r-mode))
-(add-to-list 'auto-mode-alist '("\\.rapport$" . poly-rapport-mode))
-(add-to-list 'auto-mode-alist '("\\.[rR]html$" . poly-html+r-mode))
-(add-to-list 'auto-mode-alist '("\\.[rR]brew$" . poly-brew+r-mode))
-(add-to-list 'auto-mode-alist '("\\.[Rr]cpp$" . poly-r+c++-mode))
-(add-to-list 'auto-mode-alist '("\\.cpp[rR]$" . poly-c++r-mode))
+(add-to-list 'auto-mode-alist '("\\.Snw\\'" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.[rR]nw\\'" . poly-noweb+r-mode))
+(add-to-list 'auto-mode-alist '("\\.[rR]md\\'" . poly-markdown+r-mode))
+(add-to-list 'auto-mode-alist '("\\.rapport\\'" . poly-rapport-mode))
+(add-to-list 'auto-mode-alist '("\\.[rR]html\\'" . poly-html+r-mode))
+(add-to-list 'auto-mode-alist '("\\.[rR]brew\\'" . poly-brew+r-mode))
+(add-to-list 'auto-mode-alist '("\\.[Rr]cpp\\'" . poly-r+c++-mode))
+(add-to-list 'auto-mode-alist '("\\.cpp[rR]\\'" . poly-c++r-mode))
 
 (provide 'poly-R)
